eeb03109b18d861a21fc621ba86a812d686eba7c
maximoyanobiobox@bitbucket.org <maxyam15@hotmail.com>
2021-09-12 21:14:08 -0300

    challenge kavak

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
-let wordsNotAllowed = ["hit"];
-let ouput = "";
-let words = [];
-
-function split(str){
-    for(var i = 0; i < str.length; i++)
-    {
+function split(words, str) {
+    for (var i = 0; i < str.length; i++) {
         words.push(str[i]);
     }
+    return words;
 }
 
-split(paragraph.split(' '));
-
-function cleanArray(arr) {
+function cleanArray(arr, wordsNotAllowed) {
     return arr
-      .map(name => name.toLowerCase().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ''))
-      .filter( ( el ) => !wordsNotAllowed.includes( el.toLowerCase().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ''), "") )
-  }
+        .map(name => name.toLowerCase().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ''))
+        .filter((el) => !wordsNotAllowed.includes(el.toLowerCase().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ''), ""))
+}
 
 function mostFrequentElement(arr) {
     let res = [];
@@ -33,31 +26,52 @@ function mostFrequentElement(arr) {
 }
 
 
-let arrayToSearch = cleanArray(words);
-console.log(arrayToSearch);
-ouput = mostFrequentElement(arrayToSearch);
-console.log(ouput);
+window.onload = function () {
+    let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+    let wordsNotAllowed = ["hit"];
+    let output = "";
+    let words = [];
+
+    words = split(words, paragraph.split(' '));
 
 
+    let arrayToSearch = cleanArray(words, wordsNotAllowed);
+    output = mostFrequentElement(arrayToSearch);
+    document.getElementById("output").textContent += output
 
-/*
-let paragraph2 = "a.";
-let wordsNotAllowed2 = [""];
-let ouput2 = "";
-let words2 = [];
+}
 
-function split2(str){
-    for(var i = 0; i < str.length; i++)
-    {
-        words2.push(str[i]);
+//Interactivo
+var wordsNotAllowed_ = [];
+
+function add() {
+    let wordNotAllowed = document.getElementById("wordNotAllowed").value;
+    if(wordNotAllowed != ""){
+        wordsNotAllowed_.push(wordNotAllowed);
+        console.log(wordsNotAllowed_);
+    }else{
+        alert("write a word");
     }
 }
 
-split2(paragraph2.split(' '));
+function emptyArray(){
+    wordsNotAllowed_ = [];
+    console.log(wordsNotAllowed_);
+}
 
-let arrayToSearch2 = cleanArray(words2);
-console.log(arrayToSearch2);
-ouput2 = mostFrequentElement(arrayToSearch2);
-console.log(ouput2);
-*/
+function check() {
 
+    let paragraph = document.getElementById("paragraph").value;
+    let words_ = [];
+    if(paragraph != "" && wordsNotAllowed_.length > 0){
+        words_ = split(words_, paragraph.split(' '));
+
+        let arrayToSearch = cleanArray(words_, wordsNotAllowed_);
+        output = mostFrequentElement(arrayToSearch);
+        document.getElementById("output2").textContent = "Most frequent word: " + output;
+    
+    }else{
+        alert("Please write a paragraph and inserts words not allowed");
+    }
+
+}
